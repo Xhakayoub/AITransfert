05521c1e73f0692081e81c22f84d051e30e95d0b
Xhakayoub <mouissi.ayoub@gmail.com>
2020-03-20 22:41:55 +0100

    treatement of recieving data

--- src/Services/ExtractDataFromWeb.php
+++ src/Services/ExtractDataFromWeb.php
@@ -22,17 +22,94 @@ class ExtractDataFromWeb
 
     public function updateData()
     {
+        $directories = array('England', 'Spain', 'Germany', 'Italy', 'France', 'CL', 'EL');
 
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://127.0.0.1:5000/all', ['timeout' => 2000.5]);
-        $data = $response->getContent();
+        $files = array(
+            'standars_data.csv', 'passing_data.csv', 'shooting_data.csv', 'timming_data.csv',
+            'gk_data.csv', 'ad_gk_data.csv', 'miscellaneous_data.csv'
+        );
 
-        // foreach ($data as $item) {
-        //     $fp = fopen("C:/wamp64/www/AiTransfert/public/" . $dir . "/standars_data.csv", "r+");
-        //     $line = fgets($fp);
-        //     fwrite($fp, $response->getContent());
-        //     fclose($fp);
-        // }
-        return $data;
+        // $client = HttpClient::create();
+        // $response = $client->request('GET', 'http://127.0.0.1:5000/all', ['timeout' => 2000.5]);
+        // $data = $response->getContent();
+
+        $data = array(
+            "Ligue-1-Stats" => array("shooting" => 'yy', "passing" => 'uu'),
+            "Champions-League-Stats" => array("timming" => 'yy', "standars" =>  'kk')
+        );
+        
+        foreach ($data as $index => $item) {
+          echo $index ."\n";
+            switch ($index) {
+                case "Ligue-1-Stats":
+                    $dir = "France";
+                    break;
+                case "Champions-League-Stats":
+                    $dir = "CL";
+                    break;
+                case "Serie-A-Stats":
+                    $dir = "Italy";
+                    break;
+                case "Europa-League-Stats":
+                    $dir = "EL";
+                    break;
+                case "Bundesliga-Stats":
+                    $dir = "Germany";
+                    break;
+                case "Premier-League-Stats":
+                    $dir = "England";
+                    break;
+                case "La-Liga-Stats":
+                    $dir = "Spain";
+                    break;
+            }
+            foreach ($item as $fakeindex => $element) {
+                switch ($fakeindex) {
+                    case "standard":
+                        $type = "standard";
+                        echo $type;
+                        break;
+                    case "playing_time":
+                        $type = "timming";
+                        echo $type;
+                        break;
+                    case "keeper":
+                        $type = "gk";
+                        echo $type;
+                        break;
+                    case "keeper_adv":
+                        $type = "ad_gk";
+                        echo $type;
+                        break;
+                    case "shooting":
+                        $type = "shooting";
+                        echo $type;
+                        break;
+                    case "passing":
+                        $type = "passing";
+                        echo $type;
+                        break;
+                    case "misc":
+                        $type = "miscellaneous";
+                        echo $type;
+                        break;
+                }
+
+               // echo $element . "\n";
+                $fp = fopen("C:/wamp64/www/AiTransfert/public/" . $dir . "/" . $type . "_data.csv", "r+");
+                $line = fgets($fp);
+                ftruncate($fp, 0);
+                fclose($fp);
+
+                $fp = fopen("C:/wamp64/www/AiTransfert/public/" . $dir . "/" . $type . "_data.csv", "r+");
+                $csv = $line . "\n" . $element;
+                //echo $csv;
+                fwrite($fp, $csv);
+              //  fwrite($fp, $element . $fakeindex);
+                fclose($fp);
+              
+            }
+        }
+        //return $data;
     }
 }

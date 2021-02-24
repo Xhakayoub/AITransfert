a98dd5cad1e47b2c685adf9cd691c8a4a8d9b066
Mouissi Ayoub <amouissi@adinpi.intra>
2021-02-24 14:06:06 +0100

    fix new service marketdata

--- src/Services/marketData.php
+++ src/Services/marketData.php
@@ -3,12 +3,16 @@
 namespace App\Services;
 
 use Symfony\Component\HttpClient\HttpClient;
-
+use App\Entity\Player;
 
 class marketData {
 
-    public function getValueByPlayer($fullName, $age, $club){
+    public function getValueByPlayer(Player $player){
 
+        $fullName = explode('/', $player->getName(), 2)[1];
+        $age = $player->getAge();
+        $club = $player->getSquad();
+        
         $client = HttpClient::create();
         $response = $client->request('GET', 'http://127.0.0.1:5000/TransfertMarekt/' . $fullName . '/' . $age . '/' .$club);
         
